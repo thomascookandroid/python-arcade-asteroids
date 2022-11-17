2b1702bea1aa1c3bd45ea5d017d12e48c1da73b6
topcat <thomascookandroid@outlook.com>
2022-11-17 23:25:25 +0000

    Start implementing RL environment for the game

---
+++ src/tensorflow/environment/asteroids.py
@@ -0,0 +1,11 @@
+# from tf_agents.environments import py_environment
+# from tf_agents.specs import array_spec
+#
+#
+# class AsteroidsEnvironment(py_environment.PyEnvironment):
+#
+#     def __init__(self):
+#         self._action_spec = array_spec.BoundedArraySpec(
+#             shape=(),
+#             dtype=np.
+#         )
